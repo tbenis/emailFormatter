7c8d614670121d82dd85bef9d7e5e63f73dec8c3
Benis Tambe <will.jenifer@yahoo.com>
2022-07-12 14:01:47 -0400

    Modified code to account for correct input

--- src/main/java/com/vmware/emailformatter.java
+++ src/main/java/com/vmware/emailformatter.java
@@ -3,11 +3,24 @@ package com.vmware;
 public class emailformatter {
 
     public static void main(String args[]){
-        if(args[0].split(" ").length > 1){
-//            int space = args[0].indexOf(" ");
-            args[0] = args[0].substring(0,args[0].indexOf(" "));
-        }
 
-        System.out.println(String.format("%s@example.com", args[0]));
+/* Test with these Outputs:
+* "Jon Snow" jon@thewall.com prints Jon Snow <jon@thewall.com>
+
+    Cersei cersei@redkeep.com prints Cersei <cersei@redkeep.com>
+
+    No input prints Please specify a name and email
+
+    Robert prints Please specify an email for Robert
+
+    Renly renly@example.com extraArg prints Renly <renly@example.com>
+* */
+
+//        if(args[0].split(" ").length > 1){
+//            args[0] = args[0].substring(0,args[0].indexOf(" "));
+//        }
+        if(args.length < 1) System.out.println("Please specify a name and email");
+        System.out.println(args[0]);
+        System.out.println(String.format("<%s>", args[1]));
     }
 }
